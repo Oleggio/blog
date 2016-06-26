f0a618efbb9089cb6aae1ac788b2fc4a976ccbab
Oleg <olegdev@gmail.com>
2016-06-26 11:14:02 +0300

    DZ6

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621055237) do
+ActiveRecord::Schema.define(version: 20160625081921) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string  "first_name"
+    t.string  "email"
+    t.string  "password"
+    t.string  "last_name"
+    t.boolean "receive_news"
   end
 
 end
